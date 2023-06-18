bcabb7ec3a1c3cfc4e50e7320a130108186c61ad
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-06-17 23:40:24 -0300

    construction of the first section of the site

--- mazda-website/src/app/page.js
+++ mazda-website/src/app/page.js
@@ -1,27 +1,37 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <section>
-      {/* Header */}
-      <header className={styles.header}>
-        <img src="https://assets.stickpng.com/images/61321e3700feb70004beb7b7.png" alt="" className={styles.mazda_logo}/>
-        <nav className={styles.nav}>
-          <a href="#">Models</a>
-          <a href="#">Innovations</a>
-          <a href="#">Brand</a>
-          <a href="#">Company</a>
-          <a href="#">Find a dealer</a>
-        </nav>
-      </header>
-      <div>
-        <span>CREATE DRIVING</span>
-        <div>
-          <a href="#">All models</a>
+    <div>
+      <section className={styles.layout}>
+        {/* Header */}
+        <header className={styles.header}>
+          <img src="/mazda-logo.png" alt="Mazda logo" className={styles.mazda_Logo} />
+          <nav className={styles.nav}>
+            <a href="#">Models</a>
+            <a href="#">Innovations</a>
+            <a href="#">Brand</a>
+            <a href="#">Company</a>
+            <a href="#">Find a dealer</a>
+          </nav>
+        </header>
+      </section>
+
+      <section>
+        {/* <div className={styles.container}> */}
+        <div className={styles.bgSizeCover}>
+          <div className={styles.hero_left}>
+            <h1 className={styles.text}>
+              CELEBRATE <br /> DRIVING
+            </h1>
+          </div>
+          <div className={styles.hero_right}>
+            <p>All models</p>
+            <img src="/icon-detail.svg" alt="" height={55}/>
+          </div>
         </div>
-      </div>
-      <img src="https://www.hdcarwallpapers.com/download/2021_mazda3_skyactiv_g_awd_turbo_hatchback_5k-3840x2160.jpg" alt="MAZDA SKYACTIV-G" />
-    </section>
+      </section>
+    </div>
+
   )
 }
