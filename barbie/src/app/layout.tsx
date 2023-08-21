c62e0e8158d7a138a333d6aec08afb0bb68befb6
Gabriel Hideaki <114426202+hideakigabriel@users.noreply.github.com>
2023-08-21 01:17:15 -0300

    style: adding favicon

--- barbie/src/app/layout.tsx
+++ barbie/src/app/layout.tsx
@@ -1,22 +1,47 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Barbie - The Movie',
-  description: 'Barbie suffers a crisis that leads her to question her world and her existence.Barbie suffers a crisis that leads her to question her world and her existence.',
-}
+  title: "Barbie - The Movie",
+  description:
+    "Barbie suffers a crisis that leads her to question her world and her existence.Barbie suffers a crisis that leads her to question her world and her existence.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
